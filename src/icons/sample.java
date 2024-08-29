fdeefc49464cf1a68187c2875e796bd1e96e5279
pavantn36918 <148556331+pavantn36918@users.noreply.github.com>
2024-08-29 10:54:54 +0530

    Add files via upload

---
+++ src/icons/sample.java
@@ -0,0 +1,10 @@
+package icons;
+
+public class sample {
+    public static void main(String[] args) {
+        for(int i=2024;i>1915;i--){
+            System.out.println(i);
+        }
+
+    }
+}
